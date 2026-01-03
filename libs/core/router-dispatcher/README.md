36fafbdfac86f931d8f6c675588a7667ca508faa
Dhruv Sharma <dhruvsharma0503@gmail.com>
2026-01-03 14:37:35 -0500

    feat(core/router): Add dispatcher

---
+++ libs/core/router-dispatcher/README.md
@@ -0,0 +1,4 @@
+# 402 Systems Frontend Router Dispatcher
+
+Simple cloudflare worker that parses query string
+and routes to correct r2 bucket for web assets
---
+++ libs/core/router-dispatcher/src/index.ts
@@ -0,0 +1,28 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const pathSegments = url.pathname.split('/').filter(Boolean);
+    const deployId = pathSegments[0]; // e.g., "marketing-blog"
+
+    if (!deployId)
+      return new Response('402systems: Please specify an app.', {
+        status: 400,
+      });
+    const environment = url.hostname.startsWith('staging')
+      ? 'staging'
+      : 'production';
+    const assetPath = pathSegments.slice(1).join('/') || 'index.html';
+    const r2Key = `${environment}/${deployId}/${assetPath}`;
+
+    const object = await env.DEPLOYMENTS_BUCKET.get(r2Key);
+    if (!object)
+      return new Response(`File not found in R2: ${r2Key}`, { status: 404 });
+
+    // 4. Return with correct headers
+    const headers = new Headers();
+    object.writeHttpMetadata(headers);
+    headers.set('Access-Control-Allow-Origin', '*');
+
+    return new Response(object.body, { headers });
+  },
+} satisfies ExportedHandler<Env>;
